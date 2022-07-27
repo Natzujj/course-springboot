8aaa35c727c0a3e5ee0d81d0190edf227cc76d2b
Natsujake <75273434+Natsujake@users.noreply.github.com>
2022-07-27 12:13:43 -0300

    Project created

---
+++ src/test/java/com/example/demo/SpringprojectoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
